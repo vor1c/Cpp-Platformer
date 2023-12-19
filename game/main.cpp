29c7352d303861974cbcf91395591fbd371a8075
voric <voricyt@gmail.com>
2023-12-19 09:09:09 +0100

    Updated main.cpp

--- game/main.cpp
+++ game/main.cpp
@@ -14,7 +14,7 @@ const int SPEED = 200; // Pixels per second
 const int FPS_CAP = 180;
 const int SCREEN_TICK_PER_FRAME = 1000 / FPS_CAP;
 
-int main(int argc, char* args[]) {
+int main(int argc, char *args[]) {
     std::cout << "Starting the game..." << std::endl;
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -22,7 +22,7 @@ int main(int argc, char* args[]) {
         return -1;
     }
 
-    SDL_Window* window = SDL_CreateWindow("Game Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+    SDL_Window *window = SDL_CreateWindow("Game Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                                           800, 600, SDL_WINDOW_FULLSCREEN_DESKTOP);
 
     if (!window) {
@@ -31,7 +31,7 @@ int main(int argc, char* args[]) {
         return -1;
     }
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (!renderer) {
         std::cerr << "Renderer could not be created! SDL Error: " << SDL_GetError() << std::endl;
         SDL_DestroyWindow(window);
@@ -47,7 +47,7 @@ int main(int argc, char* args[]) {
         return -1;
     }
 
-    TTF_Font* font = TTF_OpenFont(fontPath.c_str(), fontSize);
+    TTF_Font *font = TTF_OpenFont(fontPath.c_str(), fontSize);
     if (!font) {
         std::cerr << "Failed to load font: " << TTF_GetError() << std::endl;
         TTF_Quit();
@@ -85,7 +85,7 @@ int main(int argc, char* args[]) {
             player.handleEvent(e);
         }
 
-        // Calculate the delta time
+        // Calculate the delta time.
         currentTick = SDL_GetTicks();
         float deltaTime = (currentTick - lastTick) / 1000.0f; // Delta time in seconds
         lastTick = currentTick;
@@ -101,8 +101,8 @@ int main(int argc, char* args[]) {
         fpsCounter.update();
         std::stringstream fpsStream;
         fpsStream << "FPS: " << std::fixed << std::setprecision(2) << fpsCounter.getFPS();
-        SDL_Surface* fpsSurface = TTF_RenderText_Solid(font, fpsStream.str().c_str(), textColor);
-        SDL_Texture* fpsTexture = SDL_CreateTextureFromSurface(renderer, fpsSurface);
+        SDL_Surface *fpsSurface = TTF_RenderText_Solid(font, fpsStream.str().c_str(), textColor);
+        SDL_Texture *fpsTexture = SDL_CreateTextureFromSurface(renderer, fpsSurface);
         SDL_Rect fpsRect = {10, 10, fpsSurface->w, fpsSurface->h};
         SDL_RenderCopy(renderer, fpsTexture, NULL, &fpsRect);
         SDL_DestroyTexture(fpsTexture);
